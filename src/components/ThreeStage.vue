633295c1832632bf679dffe81e378826e9555198
Dan Koenig <dan.koenig@helloworld.com>
2021-11-20 18:52:34 -0500

    add custom glp file and some rotation code

--- src/components/ThreeStage.vue
+++ src/components/ThreeStage.vue
@@ -7,7 +7,8 @@
 <script>
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-const glbFile = 'https://danielpatrickkoenig.github.io/three-game-exparament/public/pencil/source/pencil.glb';
+const glbFile = 'https://danielpatrickkoenig.github.io/three-game-exparament/public/Xbot.glb';
+// const glbFile = 'https://danielpatrickkoenig.github.io/three-game-exparament/public/pencil/source/pencil.glb';
 export default {
     data () {
         return {
@@ -17,11 +18,26 @@ export default {
 
         }
     },
+    methods: {
+        styleMeshes (scope) {
+            scope.children.forEach(item => {
+                if(item.children.length){
+                    this.styleMeshes(item);
+                }
+                else{
+                    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+                    item.material = material;
+                    console.log(item);
+                }
+            });
+        }
+    },
     mounted () {
         const width = 1000;
         const height = 700;
         console.log(this.$refs.stage);
         this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color( 0xEEEEEE );
         this.camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
 
         this.renderer = new THREE.WebGLRenderer();
@@ -33,15 +49,27 @@ export default {
         // const cube = new THREE.Mesh( geometry, material );
         // this.scene.add( cube );
 
-        this.camera.position.z = 5;
+        this.camera.position.z = 4;
 
         const loader = new GLTFLoader();
 
-        loader.load( glbFile, function ( gltf ) {
-            console.log(gltf);
-            this.scene.add( gltf.scene );
+        loader.load( glbFile, ( gltf ) => {
+            console.log(gltf.scene.children);
+            const item   = gltf.scene;
+            this.scene.add( item );
+            let rot = 1;
+            setInterval(() => {
+                // rot += .1;
+                item.rotateY( rot * (Math.PI/180) );
+                this.renderer.render(this.scene, this.camera);
+            });
+            item.rotateY( 1.2 );
+            console.log(this.scene);
+            
+            // this.styleMeshes(this.scene);
+            this.renderer.render(this.scene, this.camera);
 
-        }, undefined, function ( error ) {
+        }, undefined, ( error ) => {
 
             console.error( error );
 
