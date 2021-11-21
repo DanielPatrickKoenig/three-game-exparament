58d8f06c394a4299f0a4e96eedd3ad1a3a09b565
Dan Koenig <dan.koenig@helloworld.com>
2021-11-20 23:13:18 -0500

    removed comments

--- src/classes/ModelLoader.js
+++ src/classes/ModelLoader.js
@@ -10,19 +10,6 @@ export default class ModelLoader{
         let model = null;
         loader.load( this.file, ( gltf ) => {
             model = gltf.scene;
-            // console.log(gltf.scene.children);
-            // const item   = gltf.scene;
-            // this.env.scene.add( item );
-            // let rot = 1;
-            // setInterval(() => {
-            //     // rot += .1;
-            //     item.rotateY( rot * (Math.PI/180) );
-            //     this.env.render();
-            // });
-            
-            // // this.styleMeshes(this.scene);
-            // this.env.render();
-
         }, undefined, ( error ) => {
             model = error;
             console.error( error );
